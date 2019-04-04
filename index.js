17683bf280d8ceab171c07896b499da27b9519a2
Jed <jedwoolley@gmail.com>
2019-04-04 19:39:38 +0800

    deleted mongodb attempt.  planning to serve from view folder

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const path = require('path');
 const expressEdge = require('express-edge');
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Post = require('./database/models/Post');
+
 const app = new express();
  
 app.use(express.static('public'));
 app.use(expressEdge);
 app.set('views', __dirname + '/views');
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+ 
 app.get('/', (req, res) => {
     res.render('index');
 });
  
-app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+app.get('/posts/new', (req, res) => {
+    res.render('create')
 });
- 
-app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
-});
- 
-app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
-});
- 
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App listening on port ${port}`));
